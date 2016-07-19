24131d446b0e7defd509dbb5bcfe591969aa9868
arickman <arickman@stanford.edu>
2016-07-19 10:32:32 -0700

    fixing polyhedra arguments

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -620,8 +620,7 @@ G4VSolid *DetectorConstruction::constructSolid(std::string baseName,
 		else {
 			//solid = new G4Box(baseName + "box", width / 2, m_CalorSizeXY / 2, thick / 2);
 			G4double a[2] = {0,5},b[2] = {0,0},c[2] = {20,20};
-			//solid = new G4Polyhedra(baseName + "box", 0, 2*pi, 6, 2, -50, {-12,12}, {0,0}, {24,24});
-			solid = new G4Polyhedra(baseName + "box", 0, 2*pi, 6, 2, 6, 2);
+			solid = new G4Polyhedra(baseName + "box", 0., 2*pi, 6, 2, {-12.,12.}, {0.,0.}, {24.,24.});
 		}
 	}
 	return solid;
