247a938342f0a0333b86ed8eb6469c6855f5bc2d
arickman <arickman@stanford.edu>
2016-07-27 16:28:53 -0700

    test

--- src/SeededGeneratorAction.cc
+++ src/SeededGeneratorAction.cc
@@ -65,10 +65,10 @@ SeededGeneratorAction::SeededGeneratorAction(G4int mod, std::string data) {
 	data_ = data;
 	G4int n_particle = 1;
 	evt_ = 0; inc_ = 0;
-	file_ = TFile::Open(data.c_str());
-	tree_  = (TTree*) file_->Get("HGCSSTree");
-	tree_->SetBranchAddress("HGCSSIncAction",&inc_);
-	tree_->SetBranchAddress("HGCSSEvent",&evt_);
+	//file_ = TFile::Open(data.c_str());
+	//tree_  = (TTree*) file_->Get("HGCSSTree");
+	//tree_->SetBranchAddress("HGCSSIncAction",&inc_);
+	//tree_->SetBranchAddress("HGCSSEvent",&evt_);
 
 	eventAction_ =
 			(EventAction*) G4RunManager::GetRunManager()->GetUserEventAction();
@@ -133,7 +133,7 @@ void SeededGeneratorAction::GeneratePrimaries(G4Event* anEvent) {
 			"e-");
 	particleGun->SetParticleDefinition(particle);
 	int currentEvt = anEvent->GetEventID();
-	tree_->GetEntry(currentEvt);
+	//tree_->GetEntry(currentEvt);
 	G4double et = 0.0;
 	CLHEP::HepRandom::restoreEngineStatus ("temp.rndm");
 	if (inc_->size() != 0){
